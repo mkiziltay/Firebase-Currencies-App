a5a8f1f08f55193bfab2bf0a5493e9358f5a5188
Mustafa KIZILTAY <mkiziltay@hotmail.com>
2022-04-12 00:14:12 +0300

    comments

--- lib/main.dart
+++ lib/main.dart
@@ -54,7 +54,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 stream: dayRef.snapshots(),
                 // What will we do downloaded datas.
                 builder: (BuildContext context, AsyncSnapshot asyncsnapshot) {
-                  if (asyncsnapshot.connectionState == // loading icon
+                  if (asyncsnapshot.connectionState == // loading progress
                       ConnectionState.waiting) {
                     return Center(child: CircularProgressIndicator());
                   }
@@ -70,9 +70,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         itemBuilder: (context, index) {
                           return LiStyle(
                                   title: titles[index],
-                                  color: compareValues(values[index],
-                                              valuesStart[index]) ==
-                                          1
+                                  color: compareValues(values[index],valuesStart[index]) ==
+                                          1 // if rising green else red color
                                       ? colorUP
                                       : colorDOWN,
                                   value: values[index]);
